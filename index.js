0bc2e701ce00f5cbf1b594e3951fb786166cffcb
Nandozzz <dwighthernandez39@gmail.com>
2023-03-15 18:00:26 -0600

    Verificacion de usuario

--- index.js
+++ index.js
@@ -2,11 +2,27 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 const mysql = require('./node_modules/mysql');
 
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+// creacion de la app o mi API
+const app = express();
+
+//configuraciones
+app.set('port',4000);
+
+
+// usando morgan para middlewares
+app.use(morgan('dev')); // para poder visualizar los estados de nuestro servidor
+app.use(express.json()); // para poder manjar los json
+app.use(cors())
+
 const connection = mysql.createConnection({
     host: 'localhost',
-    database: 'db_',
+    database: 'estudiantes_bd',
     user: 'root',
-    password:''
+    password:'hernandez15'
 });
 connection.connect((error)=>{
     if(error){
@@ -18,21 +34,92 @@ connection.connect((error)=>{
 });
 
 
-let name = 'estudiante2';
-let lastname = 'apellido2';
-let edad = 25;
-let sql_insert = `INSERT INTO estudiantes(name, lastname, edad) VALUES('${name}', '${lastname}', ${edad});`;
 
-connection.query(sql_insert);
+const sql_selection = `SELECT * FROM estudiantes_bd.usuarios;`;
 
-const sql_selection = `SELECT * FROM estudiantes;`;
 connection.query(sql_selection, (err, result, fields)=>{
     if(err){
         console.log(`Hubo un error ${err}`);
         return;
     }
-    console.log(result);
+    console.log(result)
+
+    app.post('/retornoUsuario',(req,res)=>{
+        let usuarios= result;
+        let existe = false
+    
+        for (let i = 0; i < usuarios.length; i++) {
+
+            if (usuarios[i].Registro_A == req.body.Registro){
+                existe= true 
+                
+                res.send({Mensaje:"Bienvenido"});
+            }        
+        }
+        
+        if(existe==false){
+            res.send({Mensaje:"No se encuentra usuario con esos datos"});
+        }
+        
+       
+    });
+
+
+
+
+
+
+    
+    
+  
 });
 
 
+
+
+
 connection.end();
+
+
+
+
+// server
+var data = [
+    {
+        Titulo: 'Saludo',
+        Mensaje:'Bienvenidos a la pagina principal',
+        Tipo:'string' 
+    },
+    {
+        Titulo: 'Saludo2',
+        Mensaje:'Bienvenida2',
+        Tipo:'string'
+    }
+];
+
+
+//routes
+app.get('/',(req,res)=>{
+    res.send('Hola mundo desde mi primer Backend con NodeJS');
+})
+
+
+app.get('/ejemplo',(req,res)=>{
+    var ejemplo = {
+        "Usuario":"Hector",
+        "Curso":"IPC1B",
+        "Horaio":"jueves"
+    }
+    res.send(ejemplo);
+})
+
+
+app.get('/home',(req,res)=>{
+    res.send(data);
+})
+
+
+
+app.listen(app.get('port'),()=>{
+    console.log('Servidor iniciado en el puerto: '+app.get('port'));
+})
